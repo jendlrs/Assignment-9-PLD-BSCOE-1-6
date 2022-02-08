ded49a7bc843ba234b64ba271d188380484aa2ed
jendlrs <jensennics.dlrs@gmail.com>
2022-02-08 19:46:06 +0800

    I changed my code into class-def function for organizing the Layout, image and body. I successfully added the json file into the python but it still need improvements for the format.

--- delarosa_resume.py
+++ delarosa_resume.py
@@ -12,29 +12,47 @@
 #	- Your code should be in github before Feb12
 
 #Import needed modules
+import os
 from fpdf import FPDF
 
 #Global variables
 dlrs_resume = ('DELAROSA_MA.JENSENNICOLE.pdf')
+json_details = ('personal_details.json')
 
 #Header
 Name = 'DELA ROSA, MA. JENSEN NICOLE C.'
 position = 'Computer Engineering Student'
 
+class PDF (FPDF):
+    def header (self):
+        #Font size and font style and format of the string for header
+        self.set_font('times','B', 20)
+        self.cell (205,30, Name)
+        self.ln(20)
+        self.set_font ('times','', 15)
+        self.cell (-137, 5, position)
+        #for images
+        self.image ('2x2_picture.png',150,10,50,0)
+        self.image ('divider.png',0,8,500,2)
+        self.image ('divider.png',0,60,500,2)
+
+    def body (self, name):
+        #Read json file
+        with open (json_details) as fh:
+            resume_data = fh.read() 
+        self.set_font ('times', "", 12)
+        self.multi_cell (0, 7,resume_data, border=0, align ="R")
+        self.ln(2)
+
+    def output_ (self, name):
+        self.body(name)
+    
 #Set size of the paper and add blank page on the file
-DelaRosa_PDF = FPDF('P', 'mm', 'Letter'); 
+DelaRosa_PDF = PDF('P', 'mm', 'Letter')
+DelaRosa_PDF.set_auto_page_break (margin =0.5, auto =True)
 DelaRosa_PDF.add_page() 
-
-#Font size and font style and format of the string for header
-DelaRosa_PDF.set_font('times', 'B', 20) 
-DelaRosa_PDF.cell(205, 15, Name) 
-DelaRosa_PDF.set_font ('times','',15)
-DelaRosa_PDF.cell (-137, 28, position, border=0,align="R", ln=2)
-
-#For images
-DelaRosa_PDF.image ('2x2_picture.png',150,10,50,0) #2x2 picture on the top right of the document
-DelaRosa_PDF.image ('divider.png',0,8,500,2) #Left, Up Length, Width
-DelaRosa_PDF.image ('divider.png',0,60,500,2) #Left, Up Length, Width
+DelaRosa_PDF.body(json_details)
 
 #Saving into PDF file
 DelaRosa_PDF.output(dlrs_resume) 
+os.startfile(dlrs_resume)
