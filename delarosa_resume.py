2f195f430276a068ad176aa967919ba40463df51
jendlrs <jensennics.dlrs@gmail.com>
2022-02-09 14:29:50 +0800

    Added academic background from json file.

--- delarosa_resume.py
+++ delarosa_resume.py
@@ -63,6 +63,17 @@ class PDF (FPDF):
         self.set_font ("times",'', 11)
         self.cell(70,7, "Email: " + str(resume_data["contactInfo"][0]["Email"]),ln =True)
         self.cell(70,7, "Cellphone Number: " + str(resume_data["contactInfo"][0]["Cellphone number"]),ln =True)
+
+    def academicBackground (self):
+        self.set_font ("times", 'B', 13)
+        self.cell (0,4, "", ln =True)
+        self.set_fill_color(152,245,255)
+        self.cell(115,8,"ACADEMIC BACKGROUND", ln =1, border= 1, fill= True, align= 'C')
+        self.set_font ("times",'', 11)
+        self.cell(70,7, "Elementary (2009-2015): " + str(resume_data["academicBackground"][0]["Elementary (2009-2015)"]),ln =True)
+        self.cell(70,7, "Junior High School (2015-1019): " + str(resume_data["academicBackground"][0]["Junior High School (2015-1019"]),ln =True)
+        self.cell(70,7, "Senior High School (2019-2021): " + str(resume_data["academicBackground"][0]["Senior High School (2019-2021)"]),ln =True)
+        self.cell(70,7, "College (PRESENT): " + str(resume_data["academicBackground"][0]["College (PRESENT)"]),ln =True)
  
 #Set size of the paper and add blank page on the file
 DelaRosa_PDF = PDF('P', 'mm', 'Letter')
@@ -72,6 +83,8 @@ DelaRosa_PDF.add_page()
 #calling the functions
 DelaRosa_PDF.primaryDetails()
 DelaRosa_PDF.contactInfo()
+DelaRosa_PDF.academicBackground()
+
 #Saving into PDF file
 DelaRosa_PDF.output(dlrs_resume) 
 os.startfile(dlrs_resume)
