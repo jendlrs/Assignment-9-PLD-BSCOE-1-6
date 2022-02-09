f4350b2429a9b108704ab12c1a994cda655dbf40
jendlrs <jensennics.dlrs@gmail.com>
2022-02-09 19:26:48 +0800

    I updated the format of the pdf file to have more spaces.

--- delarosa_resume.py
+++ delarosa_resume.py
@@ -38,31 +38,31 @@ class PDF (FPDF):
         self.set_font ('times','', 15)
         self.cell (-137, 5, position)
         #for images
-        self.image ('2x2_picture.png',150,10,50,0)
+        self.image ('2x2_picture.png',160,10,40,0)
         self.image ('divider.png',0,8,500,2)
-        self.image ('divider.png',0,60,500,2)
+        self.image ('divider.png',0,50,500,2)
 
     def primaryDetails(self):
         self.set_font("times", "B", 13)
-        self.cell (0,35, "", ln=True)
+        self.cell (0,25, "", ln=True)
         self.set_fill_color(152,245,255)
-        self.cell(115,8,"PERSONAL INFORMATION", ln =1, border= 1, fill= True, align= 'C')
+        self.cell(195,8,"PERSONAL INFORMATION", ln =1, border= 1, fill= True, align= 'C')
         self.set_font ("times",'', 11)
-        self.cell(70,7, "Full Name: " + str(resume_data["primaryDetails"][0]["Full Name"]),ln =True)
-        self.cell(70,7, "Sex/Gender: " + str(resume_data["primaryDetails"][0]["Sex / Gender"]),ln =True)
-        self.cell(70,7, "Age: " + str(resume_data["primaryDetails"][0]["Age"]),ln =True)
-        self.cell(70,7, "Home Address: " + str(resume_data["primaryDetails"][0]["Home Address"]),ln =True)
-        self.cell(70,7, "Height: " + str(resume_data["primaryDetails"][0]["Height"]),ln =True)
-        self.cell(70,7, "Weight: " + str(resume_data["primaryDetails"][0]["Weight"]),ln =True)
+        self.cell(90,7, "Full Name: " + str(resume_data["primaryDetails"][0]["Full Name"]),ln =False, align = 'R')
+        self.cell(80,7, "Sex/Gender: " + str(resume_data["primaryDetails"][0]["Sex / Gender"]),ln =True, align = 'R')
+        self.cell(121,7, "Home Address: " + str(resume_data["primaryDetails"][0]["Home Address"]),ln =False, align ='R')
+        self.cell(40,7, "Age: " + str(resume_data["primaryDetails"][0]["Age"]),ln =True, align= 'R')
+        self.cell(45,7, "Height: " + str(resume_data["primaryDetails"][0]["Height"]),ln =False, align = 'R')
+        self.cell(109,7, "Weight: " + str(resume_data["primaryDetails"][0]["Weight"]),ln =True, align= 'R')
     
     def contactInfo (self):
-        self.set_font ("times", 'B', 13)
-        self.cell (0,4, "", ln =True)
+        self.set_font("times", "B",13)
+        self.cell (0,4,"",ln =True)
         self.set_fill_color(152,245,255)
-        self.cell(115,8,"CONTACT INFORMATION", ln =1, border= 1, fill= True, align= 'C')
+        self.cell (195, 8, "CONTACT INFORMATION", border=1, fill=True, align = 'C', ln=1)
         self.set_font ("times",'', 11)
-        self.cell(70,7, "Email: " + str(resume_data["contactInfo"][0]["Email"]),ln =True)
-        self.cell(70,7, "Cellphone Number: " + str(resume_data["contactInfo"][0]["Cellphone number"]),ln =True)
+        self.cell(69,7, "Email: " + str(resume_data["contactInfo"][0]["Email"]),ln =False, align = 'R')
+        self.cell(116,7, "Cellphone Number: " + str(resume_data["contactInfo"][0]["Cellphone number"]),ln =True, align = 'R')
 
     def academicBackground (self):
         self.set_font ("times", 'B', 13)
@@ -88,7 +88,16 @@ class PDF (FPDF):
         self.cell(70,7, str(resume_data["achievements"][0]["Achvmnts3_2"]),ln =True)
         self.cell(70,7, str(resume_data["achievements"][0]["Achvmnts3_3"]),ln =True)
 
- 
+    def coreCompetencies (self):
+        self.set_font ("times", 'B', 13)
+        self.cell (0,4, "", ln =True)
+        self.set_fill_color(152,245,255)
+        self.cell(115,8,"CORE COMPETENCIES", ln =1, border= 1, fill= True, align= 'C')
+        self.set_font ("times",'', 11)
+        self.cell(70,7, str(resume_data["coreCompetencies"][0]["Competencies1"]),ln =True)
+        self.cell(70,7, str(resume_data["coreCompetencies"][0]["Competencies2"]),ln =True)
+        self.cell(70,7, str(resume_data["coreCompetencies"][0]["Competencies3"]),ln =True)
+
 #Set size of the paper and add blank page on the file
 DelaRosa_PDF = PDF('P', 'mm', 'Letter')
 DelaRosa_PDF.set_auto_page_break (margin =0.5, auto =True)
@@ -99,6 +108,8 @@ DelaRosa_PDF.primaryDetails()
 DelaRosa_PDF.contactInfo()
 DelaRosa_PDF.academicBackground()
 DelaRosa_PDF.achievements()
+DelaRosa_PDF.coreCompetencies()
+
 #Saving into PDF file
 DelaRosa_PDF.output(dlrs_resume) 
 os.startfile(dlrs_resume)
