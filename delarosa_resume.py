8fb05a304f248956c7fb80855a923a88ad0f3705
jendlrs <jensennics.dlrs@gmail.com>
2022-02-09 14:51:05 +0800

    Added the achievements details from json.

--- delarosa_resume.py
+++ delarosa_resume.py
@@ -74,6 +74,20 @@ class PDF (FPDF):
         self.cell(70,7, "Junior High School (2015-1019): " + str(resume_data["academicBackground"][0]["Junior High School (2015-1019"]),ln =True)
         self.cell(70,7, "Senior High School (2019-2021): " + str(resume_data["academicBackground"][0]["Senior High School (2019-2021)"]),ln =True)
         self.cell(70,7, "College (PRESENT): " + str(resume_data["academicBackground"][0]["College (PRESENT)"]),ln =True)
+
+    def achievements(self):
+        self.set_font ("times", 'B', 13)
+        self.cell (0,4, "", ln =True)
+        self.set_fill_color(152,245,255)
+        self.cell(115,8,"ACHIEVEMENTS", ln =1, border= 1, fill= True, align= 'C')
+        self.set_font ("times",'', 11)
+        self.cell(70,7, str(resume_data["achievements"][0]["Achvmnts1"]),ln =True)
+        self.cell(70,7, str(resume_data["achievements"][0]["Achvmnts2"]),ln =True)
+        self.cell(70,7, str(resume_data["achievements"][0]["Achvmnts3"]),ln =True)
+        self.cell(70,7, str(resume_data["achievements"][0]["Achvmnts3_1"]),ln =True)
+        self.cell(70,7, str(resume_data["achievements"][0]["Achvmnts3_2"]),ln =True)
+        self.cell(70,7, str(resume_data["achievements"][0]["Achvmnts3_3"]),ln =True)
+
  
 #Set size of the paper and add blank page on the file
 DelaRosa_PDF = PDF('P', 'mm', 'Letter')
@@ -84,7 +98,7 @@ DelaRosa_PDF.add_page()
 DelaRosa_PDF.primaryDetails()
 DelaRosa_PDF.contactInfo()
 DelaRosa_PDF.academicBackground()
-
+DelaRosa_PDF.achievements()
 #Saving into PDF file
 DelaRosa_PDF.output(dlrs_resume) 
 os.startfile(dlrs_resume)
